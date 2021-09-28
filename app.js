12274864f77ebcfa17d749a8f4e7a917a5d001a7
Thomas Norris <tom.norris135@gmail.com>
2021-09-28 11:01:10 -0400

    finish import

---
+++ app.js
@@ -0,0 +1,5 @@
+var _path = require('path');
+var _assistant = require(_path.resolve(__dirname, 'REST-GoogleAssistant-Client', 'client.js'));
+var _logger = require(_path.resolve(__dirname, 'Node-Logger', 'app.js'));
+
+console.log('ready');
