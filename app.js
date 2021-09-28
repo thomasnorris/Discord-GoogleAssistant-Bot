b3d2d90e9bf6f4da791045bc12b0b6452dc91373
Thomas Norris <tom.norris135@gmail.com>
2021-09-28 13:27:11 -0400

    only reply if the bot is mentioned

--- app.js
+++ app.js
@@ -4,18 +4,32 @@ var _logger = require(_path.resolve(__dirname, 'Node-Logger', 'app.js'));
 
 var _discord = require('discord.js');
 var _bot = new _discord.Client({
-    intents: [_discord.Intents.FLAGS.GUILDS]
+    intents: [_discord.Intents.FLAGS.GUILDS, _discord.Intents.FLAGS.GUILD_MESSAGES, _discord.Intents.FLAGS.DIRECT_MESSAGES]
 });
 
 const CFG_FILE = _path.resolve(__dirname, 'config', 'config.json');
 var _cfg = readJson(CFG_FILE);
 
+_bot.on('ready', handleReady);
+_bot.on('messageCreate', handleMessageCreate);
+
+// start
 _bot.login(_cfg.token);
 
-_bot.on('ready', () => {
+function handleMessageCreate(msg) {
+    if (botMentioned(msg)) {
+        msg.reply('You said: ' + msg.content);
+    }
+}
+
+function handleReady() {
     console.log('Ready!');
     _logger.Info.Async('Logged in', 'User: ' + _bot.user.tag);
-});
+}
+
+function botMentioned(msg) {
+    return msg.content.startsWith('<@') && msg.content.includes(_bot.user.id);
+}
 
 function readJson(filePath) {
     var fs = require('fs');
